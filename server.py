c1ae956b456df76cb24335e335d636b0f66eab6c
Herklos <herklos.dev@protonmail.com>
2023-10-16 11:01:49 +0200

    [Server] Restore cache

--- server.py
+++ server.py
@@ -2,13 +2,17 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_caching import Cache
 
 import triangular_arbitrage.detector
 
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 app = Flask(__name__)
+cache.init_app(app)
 
 @app.route("/", defaults={'exchange': 'binance'})
 @app.route("/<exchange>")
+@cache.cached(timeout=os.getenv('CACHE_TIME', 60*60)) # cache it 1h by default
 def get_data(exchange):
     # start arbitrage detection
     print("Scanning...")
